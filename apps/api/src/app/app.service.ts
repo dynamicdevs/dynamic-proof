e0ae5dab4fa7ac62730918e71f9458b827322f02
Milagros O. Almidon <orihuela.ms@gmail.com>
2022-06-02 16:54:48 -0500

    fix: remove unnecesary code

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,23 +1,3 @@
-import { Inject, Injectable } from '@nestjs/common';
-
-import { ConfigType } from '@nestjs/config';
-import config from '@env';
-
+import { Injectable } from '@nestjs/common';
 @Injectable()
-export class AppService {
-  private serviceAccountEmail: string;
-  private serviceAccountPrivateKey: string;
-  private scopes: string[] = [];
-  private spreadsheetId: string;
-
-  constructor(
-    @Inject(config.KEY) private configService: ConfigType<typeof config>
-  ) {
-    this.serviceAccountEmail = this.configService.serviceAccountEmail;
-    this.serviceAccountPrivateKey = decodeURI(
-      this.configService.serviceAccountPrivateKey
-    );
-    this.spreadsheetId = this.configService.spreadsheetId;
-    this.scopes = ['https://www.googleapis.com/auth/spreadsheets'];
-  }
-}
+export class AppService {}
