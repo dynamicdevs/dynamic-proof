e593868bbc8f4a4ec379f40ec77fbf8d63e1f45b
César R. Contreras <c1sar90@gmail.com>
2022-06-16 12:30:08 -0500

    chore: update dependencies

--- apps/proof/src/app/app.module.ts
+++ apps/proof/src/app/app.module.ts
@@ -9,9 +9,16 @@ import { AppRoutingModule } from './app.routing';
 import { CertificateComponent } from './certificate/certificate.component';
 import { SymbolsModule } from '@dynamic-devs/symbol-angular';
 
+import { ShareModule } from 'ngx-sharebuttons';
+
+const LIBRARIES = [SymbolsModule, ShareModule];
+
 @NgModule({
   declarations: [AppComponent, CertificateComponent, NxWelcomeComponent],
-  imports: [BrowserModule, HttpClientModule, ElementsModule, AppRoutingModule, SymbolsModule],
+  imports: [
+    BrowserModule, HttpClientModule, ElementsModule, AppRoutingModule,
+    ...LIBRARIES
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- apps/proof/src/app/certificate/certificate-mock.ts
+++ apps/proof/src/app/certificate/certificate-mock.ts
@@ -6,6 +6,8 @@ export const CERTIFICATE: Certificate = {
   lastname: 'Contreras',
   headline: 'Certificado de Participación',
   subtitle: 'Perú  |  Equipo Dynamic Devs',
+  issueDate: '25/04/2020',
+  eventName: 'Hackatón Dynamic Devs 2022',
   description: 'Este certificado reconoce la participación y completición de las actividades realizadas en la Hackatón Dynamic Devs 2022.',
   certificateImgUrl: '001/d49f7cfb-ebd2-4849-9111-442fd1bcd258/Milagros-Oregon-Hackaton-Dynamic-Devs-2022.jpg',
   certificatePdfUrl: '001/d49f7cfb-ebd2-4849-9111-442fd1bcd258/Milagros-Oregon-Hackaton-Dynamic-Devs-2022.pdf'
